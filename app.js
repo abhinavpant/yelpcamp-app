9b3484ce3f71a611f1b2d1c9826c0d7429cd3fe5
abhinavpant <141048769+abhinavpant@users.noreply.github.com>
2024-01-16 22:53:35 +0530

    first commit

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const userRoutes = require('./Routes/users')
 const campgroundRoutes =require('./Routes/campgrounds')
 const reviewRoutes =  require('./Routes/review')
 const helmet = require('helmet');
+const MongoStore = require('connect-mongo')(session);
 const dbUrl = process.env.DB_URL || 'mongodb://0.0.0.1:27017/yelpCamp'
 
 mongoose.connect(dbUrl);
