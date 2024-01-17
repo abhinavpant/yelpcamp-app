c11da0a5766560cee49ade8b59f972ab371d8d40
abhinavpant <141048769+abhinavpant@users.noreply.github.com>
2024-01-18 05:02:32 +0530

    Fifth commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user')
-const helmet = require('helmet');
+//const helmet = require('helmet');
 
 const mongoSanitize = require('express-mongo-sanitize');
 
@@ -22,7 +22,8 @@ const campgroundRoutes =require('./Routes/campgrounds')
 const userRoutes = require('./Routes/users')
 const reviewRoutes =  require('./Routes/review')
 const MongoStore = require('connect-mongo');
-const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelpCamp'
+//process.env.DB_URL ||
+const dbUrl =  'mongodb://localhost:27017/yelpCamp'
 
 mongoose.connect(dbUrl);
 
@@ -40,30 +41,29 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-
 app.use(express.static(path.join(__dirname,'public')))
-app.use(mongoSanitize({
+/*app.use(mongoSanitize({
     replaceWith: '_'
-}))
+}))*/
 
-const secret = process.env.SECRET || 'thisshouldbeabettersecret!' ;
+//const secret = process.env.SECRET || 'thisshouldbeabettersecret!' ;
 
-const store = MongoStore.create({
+/*const store = MongoStore.create({
     mongoUrl: dbUrl,
     touchAfter: 24 * 60 * 60,
     crypto: {
         secret
     }
-});
+});*/
 
-store.on("error", function (e) {
+/*store.on("error", function (e) {
     console.log("SESSION STORE ERROR", e)
-})
+})*/
 
 const sessionConfig = {
-    store,
+    //store,
+    secret: 'thisshouldbeabettersecret!',
     name: 'session',
-    secret,
     resave: false,
     saveUninitialized: true,
     cookie :{
@@ -75,9 +75,9 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig))
 app.use(flash())
-app.use(helmet());
+//app.use(helmet());
 
-const scriptSrcUrls = [
+/*const scriptSrcUrls = [
     "https://stackpath.bootstrapcdn.com/",
     "https://api.tiles.mapbox.com/",
     "https://api.mapbox.com/",
@@ -121,7 +121,7 @@ app.use(
         },
     })
 );
-
+*/
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -160,6 +160,6 @@ res.status(statusCode).render('error', { err })
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Serving on Port ${port}`)
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
 })
