c013c617d7cb31c0b26bb734e7272247f8f553e7
abhinavpant <141048769+abhinavpant@users.noreply.github.com>
2024-02-02 20:01:02 +0530

    vercelvaali64

--- schemas.js
+++ schemas.js
@@ -1,7 +1,6 @@
-const BaseJoi = require('joi');
-const sanitizeHtml = require('sanitize-html');
+const Joi = require('joi');
 
-const extension = (joi) => ({
+/*const extension = (joi) => ({
     type: 'string',
     base: joi.string(),
     messages: {
@@ -19,16 +18,16 @@ const extension = (joi) => ({
             }
         }
     }
-});
+});*/
 
-const Joi = BaseJoi.extend(extension)
+//const Joi = BaseJoi.extend(extension)
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
-        title: Joi.string().required().escapeHTML(),
+        title: Joi.string().required(),//.escapeHTML()
         price: Joi.number().required().min(0),
         //image: Joi.string().required(),
-        location: Joi.string().required().escapeHTML(),
-        description: Joi.string().required().escapeHTML()
+        location: Joi.string().required(),//.escapeHTML()
+        description: Joi.string().required()//.escapeHTML()
     }).required(),
     deleteImages: Joi.array()
 });
@@ -36,6 +35,6 @@ module.exports.campgroundSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
-        body: Joi.string().required().escapeHTML()
+        body: Joi.string().required()//.escapeHTML()
     }).required()
 })
